4641e4c3c703eac4ceea8c523c6e84cab50d8791
Shreeyash Sunil Rakibe <shreeyashrakibe18@gmail.com>
2024-12-18 13:36:59 +0530

    Add files via upload

---
+++ PuneRto.java
@@ -0,0 +1,18 @@
+public class PuneRto {
+    
+
+    int puneRto(int Age){
+
+        if (Age >= 18 && Age <=75){
+
+            System.out.println("Your age is " + Age + " Welcome to Pune RTO you are Eligible to Licence");
+        }
+    
+    
+    else {
+        System.out.println("Your age is " + Age + " Sorry you are not Applicable for Licence");
+
+    }
+return Age;
+}
+}
---
+++ TestPuneRto.java
@@ -0,0 +1,9 @@
+public class TestPuneRto {
+    
+    public static void main(String[] args) {
+        
+    PuneRto R1 = new PuneRto();
+    int Result = R1.puneRto(89);
+
+    }
+}
